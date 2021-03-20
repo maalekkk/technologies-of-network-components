0299461bd45adc7a06946f48bd9823df8da13b8b
Bartlomiej Malkowski <215817@edu.p.lodz.pl>
2021-03-20 21:26:51 +0100

    implement RentRepositoryTest

---
+++ TKS/RepositoriesAdapters/src/test/java/pl/lodz/pl/tks/repositoriesadapters/repository/RentRepositoryTest.java
@@ -0,0 +1,129 @@
+package pl.lodz.pl.tks.repositoriesadapters.repository;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.lodz.p.tks.repositoriesadapters.data.machine.MachineGamingEnt;
+import pl.lodz.p.tks.repositoriesadapters.data.rent.PeriodEnt;
+import pl.lodz.p.tks.repositoriesadapters.data.rent.RentEnt;
+import pl.lodz.p.tks.repositoriesadapters.data.user.UserEnt;
+import pl.lodz.p.tks.repositoriesadapters.repository.RentRepository;
+
+import java.util.UUID;
+
+public class RentRepositoryTest {
+
+    private RentRepository rentRepository;
+
+    @Before
+    public void init() {
+        rentRepository = new RentRepository();
+    }
+
+    @Test
+    public void findAllTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.findAll().get(0), rent);
+        Assert.assertFalse(rentRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void findByIdTest() {
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.findById(rent.getId()).orElseThrow(), rent);
+        Assert.assertTrue(rentRepository.findById(UUID.randomUUID()).isEmpty());
+    }
+
+    @Test
+    public void existByIdTest() {
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertTrue(rentRepository.existsById(rent.getId()));
+        Assert.assertFalse(rentRepository.existsById(UUID.randomUUID()));
+    }
+
+    @Test
+    public void countTest() {
+        Assert.assertEquals(rentRepository.count(), 0);
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+    }
+
+    @Test
+    public void saveTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+        Assert.assertEquals(rentRepository.findAll().get(0), rent);
+    }
+
+    @Test
+    public void updateTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+        Assert.assertEquals(rentRepository.findAll().get(0), rent);
+        PeriodEnt period = new PeriodEnt();
+        rent.setPeriod(period);
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+        Assert.assertEquals(rentRepository.findAll().get(0).getPeriod(), period);
+    }
+
+    @Test
+    public void deleteTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+        rentRepository.delete(rent);
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        rentRepository.save(rent);
+        Assert.assertEquals(rentRepository.count(), 1);
+        rentRepository.deleteById(rent.getId());
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void deleteAllTest() {
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+        UserEnt user = new UserEnt("user", "user user", true);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent = new RentEnt(machine, user, new PeriodEnt());
+        MachineGamingEnt machine2 = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        RentEnt rent2 = new RentEnt(machine2, user, new PeriodEnt());
+        rentRepository.save(rent);
+        rentRepository.save(rent2);
+        Assert.assertEquals(rentRepository.count(), 2);
+        rentRepository.deleteAll();
+        Assert.assertTrue(rentRepository.findAll().isEmpty());
+    }
+}
