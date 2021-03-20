709a33dd8ff83e2dbaa38e579abb8f47f25fd13f
Bartlomiej Malkowski <215817@edu.p.lodz.pl>
2021-03-20 21:14:03 +0100

    implement MachineTests fix problem with annotation in entities

--- TKS/RepositoriesAdapters/src/test/java/pl/lodz/pl/tks/repositoriesadapters/repository/MachineRepositoryTest.java
+++ TKS/RepositoriesAdapters/src/test/java/pl/lodz/pl/tks/repositoriesadapters/repository/MachineRepositoryTest.java
@@ -1,4 +1,107 @@
 package pl.lodz.pl.tks.repositoriesadapters.repository;
 
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.lodz.p.tks.repositoriesadapters.data.machine.MachineGamingEnt;
+import pl.lodz.p.tks.repositoriesadapters.repository.MachineRepository;
+
+import java.util.UUID;
+
 public class MachineRepositoryTest {
+
+    private MachineRepository machineRepository;
+
+    @Before
+    public void init() {
+        machineRepository = new MachineRepository();
+    }
+
+    @Test
+    public void findAllTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.findAll().get(0), machine);
+        Assert.assertFalse(machineRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void findByIdTest() {
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.findById(machine.getId()).orElseThrow(), machine);
+        Assert.assertTrue(machineRepository.findById(UUID.randomUUID()).isEmpty());
+    }
+
+    @Test
+    public void existByIdTest() {
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertTrue(machineRepository.existsById(machine.getId()));
+        Assert.assertFalse(machineRepository.existsById(UUID.randomUUID()));
+    }
+
+    @Test
+    public void countTest() {
+        Assert.assertEquals(machineRepository.count(), 0);
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+    }
+
+    @Test
+    public void saveTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+        Assert.assertEquals(machineRepository.findAll().get(0), machine);
+    }
+
+    @Test
+    public void updateTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+        Assert.assertEquals(machineRepository.findAll().get(0), machine);
+        machine.setName("new machine");
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+        Assert.assertEquals(machineRepository.findAll().get(0).getName(), "new machine");
+    }
+
+    @Test
+    public void deleteTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+        machineRepository.delete(machine);
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        Assert.assertEquals(machineRepository.count(), 1);
+        machineRepository.deleteById(machine.getId());
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+    }
+
+    @Test
+    public void deleteAllTest() {
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+        MachineGamingEnt machine = new MachineGamingEnt("machine", 8, 256, 256, 3000, 200);
+        MachineGamingEnt machine2 = new MachineGamingEnt("machine2", 8, 256, 256, 3000, 200);
+        machineRepository.save(machine);
+        machineRepository.save(machine2);
+        Assert.assertEquals(machineRepository.count(), 2);
+        machineRepository.deleteAll();
+        Assert.assertTrue(machineRepository.findAll().isEmpty());
+    }
 }
