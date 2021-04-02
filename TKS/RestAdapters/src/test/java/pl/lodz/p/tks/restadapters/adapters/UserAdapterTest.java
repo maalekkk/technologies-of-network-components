ec1375c3c2661229423f1bea1c62206397b05729
224322 Karski Dominik <224322@edu.p.lodz.pl>
2021-04-02 17:03:35 +0200

    New tests in UserAdapterTest has been created.

--- TKS/RestAdapters/src/test/java/pl/lodz/p/tks/restadapters/adapters/UserAdapterTest.java
+++ TKS/RestAdapters/src/test/java/pl/lodz/p/tks/restadapters/adapters/UserAdapterTest.java
@@ -1,20 +1,22 @@
 package pl.lodz.p.tks.restadapters.adapters;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import io.restassured.RestAssured;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.primefaces.shaded.json.JSONArray;
+import org.primefaces.shaded.json.JSONObject;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.lodz.p.tks.view.domainmodel.model.user.User;
 
-import java.io.IOException;
 import java.nio.file.Path;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
 
 @Testcontainers
 public class UserAdapterTest {
@@ -28,20 +30,100 @@ public class UserAdapterTest {
             .withExposedPorts(8080, 4848)
             .waitingFor(Wait.forHttp("/VMRental/api/users").forPort(8080).forStatusCode(200));
 
-    static HttpClient client;
-    static String baseUrl;
 
     @BeforeClass
     public static void setupClass() {
         app.start();
-        client = HttpClientBuilder.create().build();
-        baseUrl = "http://localhost:" + app.getMappedPort(8080) + "/VMRental/api/users";
+
+        RestAssured.baseURI = "http://localhost:" + app.getMappedPort(8080) + "/VMRental/api/users";
+    }
+
+
+    @Test
+    public void getAllTest() {
+        var res = given()
+                .when()
+                .get();
+        res.then()
+                .contentType("application/json")
+                .statusCode(200);
+
+        var jsonArray = new JSONArray(res.body().asString());
+        Assert.assertEquals(jsonArray.length(), 3);
     }
 
     @Test
-    public void getAllTest() throws IOException {
-        HttpGet getRequest = new HttpGet(baseUrl);
-        HttpResponse response = client.execute(getRequest);
-        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+    public void getUserByIdTest() {
+        String userName = "Blazz";
+
+        var res = given()
+                .when()
+                .queryParam("username", userName)
+                .get("/user");
+        res.then()
+                .contentType("application/json")
+                .statusCode(200);
+        var jsonObj = new JSONObject(res.body().asString());
+
+        String userId = jsonObj.getString("id");
+
+        var res2 = given()
+                .when()
+                .get("/" + userId);
+        res2.then()
+                .contentType("application/json")
+                .statusCode(200);
+
+        var jsonObj2 = new JSONObject(res.body().asString());
+
+        Assert.assertEquals(jsonObj.getString("username"), jsonObj2.getString("username"));
+    }
+
+    @Test
+    public void getUserByNameTest() {
+        String userName = "Blazz";
+
+        var res = given()
+                .when()
+                .queryParam("username", userName)
+                .get("/user");
+        res.then()
+                .contentType("application/json")
+                .statusCode(200);
+
+        var jsonObj = new JSONObject(res.body().asString());
+        Assert.assertEquals(jsonObj.get("fullname"), "Maciej Błażewicz");
+    }
+
+    @Test
+    public void addUserTest() {
+        User newUser = new User("testo", "Lukas Zimmerman", true);
+        newUser.setId(UUID.randomUUID());
+
+        var res = given()
+                .contentType("application/json")
+                .body(new JSONObject(newUser).toString())
+                .when().post();
+        res.then()
+                .statusCode(200);
+        // TODO Expected status code <200> but was <500>.
+    }
+
+    @Test
+    public void updateUserByIdTest() {
+//        String userName = "Blazz";
+//
+//        var res = given()
+//                .when()
+//                .queryParam("username", userName)
+//                .get("/user");
+//        res.then()
+//                .contentType("application/json")
+//                .statusCode(200);
+//        var jsonObj = new JSONObject(res.body().asString());
+//
+//        String userId = jsonObj.getString("id");
+
+        //TODO
     }
 }
